037a1cf8ac3d8f73cdb60f78cc307b82fcd5308c
Tania Makhija <taniamakhija18@gmail.com>
2025-08-12 11:11:51 -0700

    Initial Commit

---
+++ EcoQuest/EcoQuestApp.swift
@@ -0,0 +1,17 @@
+//
+//  EcoQuestApp.swift
+//  EcoQuest
+//
+//  Created by Scholar on 8/12/25.
+//
+
+import SwiftUI
+
+@main
+struct EcoQuestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
