9ab578668a020399b53ee9102d079518fbde7e20
Vianna Moshtaghi <vianna.mosh@gmail.com>
2025-08-12 11:46:22 -0700

    merge conflict needed

--- EcoQuest/ContentView.swift
+++ EcoQuest/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("BYEEEEEE, world! vianna rulez")
             Text("Change 1")
             HStack{
                 Text("HIIII")
