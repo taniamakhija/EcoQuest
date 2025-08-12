9dc87152902a3ac2705bd37f0e40a6b165f1343c
Vianna Moshtaghi <vianna.mosh@gmail.com>
2025-08-12 11:50:24 -0700

    now ready to actually code #freshstart

--- EcoQuest/ContentView.swift
+++ EcoQuest/ContentView.swift
@@ -9,29 +9,8 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world! - zoe")
-            Text("BYEEEEEE, world! vianna rulez")
-            
-            Text("Change 1")
-            HStack{
-                Text("HIIII")
-                    .font(.title)
-                Text("ok bru")
-            }
-            Text("Vianna Change HAHA")
-            Text("new change - Tania")
-            
-            Text("we went a merge conflict")
-            
-            Text("test 2 lets figure this out")
-            
-            Text("Change 3")
-        }
-        .padding()
+        
+       
     }
 }
 
