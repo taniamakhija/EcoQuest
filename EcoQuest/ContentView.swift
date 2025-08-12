12245ae13f0f5f8071b409bf96251a19606564bf
Tania Makhija <taniamakhija18@gmail.com>
2025-08-12 13:11:44 -0700

    home page + buttons

--- EcoQuest/ContentView.swift
+++ EcoQuest/ContentView.swift
@@ -9,8 +9,61 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        
-       
+        NavigationStack {
+            VStack {
+                Text("Welcome to EcoQuest!")
+                    .font(.title)
+                    .fontWeight(.semibold)
+                    .padding()
+                
+               //log page
+                NavigationLink(destination:
+                    VStack {
+                        Text("Log Page")
+                            .fontWeight(.semibold)
+                            .multilineTextAlignment(.center)
+                    }
+                    .navigationTitle("Home")
+                    .navigationBarTitleDisplayMode(.inline)
+                ) {
+                    Text("Log")
+                        .font(.title2)
+                        .multilineTextAlignment(.center)
+                }
+                
+                
+                //event page
+                NavigationLink(destination:
+                    VStack {
+                        Text("Event Page")
+                            .fontWeight(.semibold)
+                            .multilineTextAlignment(.center)
+                    }
+                    .navigationTitle("Home")
+                    .navigationBarTitleDisplayMode(.inline)
+                ) {
+                    Text("Event")
+                        .font(.title2)
+                        .multilineTextAlignment(.center)
+                }
+                
+                //learn page
+                NavigationLink(destination:
+                    VStack {
+                        Text("Learn Page")
+                            .fontWeight(.semibold)
+                            .multilineTextAlignment(.center)
+                    }
+                    .navigationTitle("Home")
+                    .navigationBarTitleDisplayMode(.inline)
+                ) {
+                    Text("Learn")
+                        .font(.title2)
+                        .multilineTextAlignment(.center)
+                }
+                
+            }
+        }
     }
 }
 
