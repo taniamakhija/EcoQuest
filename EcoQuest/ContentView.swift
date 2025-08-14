2acdd88fdd47064cf4d14ce501f9cacafc59bb16
Vianna Moshtaghi <vianna.mosh@gmail.com>
2025-08-14 14:13:19 -0700

    .

--- EcoQuest/ContentView.swift
+++ EcoQuest/ContentView.swift
@@ -100,6 +100,7 @@ struct ContentView: View {
                 }
                 
                 
+                
                 //learn page
                 NavigationLink(){
                     LearnPage()
